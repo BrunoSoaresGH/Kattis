1fedf011569dcb3ce65912a95997cc2138d0f4fa
Bruno Soares <36804850+BrunoSoaresGH@users.noreply.github.com>
2020-01-02 21:13:55 -0300

    SolvingForCarrots.java

---
+++ SolvingForCarrots.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SolvingForCarrots {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int contestants = s.nextInt();
+		int problems = s.nextInt();
+		
+		for(int i = 0; i < contestants; i++) {
+			String description = s.next();
+		}
+		
+		System.out.println(problems);
+	}
+
+}
